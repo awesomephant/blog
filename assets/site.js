3f947960c5f81188cc29b4e4fe3d319515b2fb06
Max Kohler <hi@maxkoehler.com>
2018-08-01 19:29:52 +0100

    Add goods section

--- assets/site.js
+++ assets/site.js
@@ -20,7 +20,7 @@ const addLineNumbers = function () {
     }
 }
 document.addEventListener('DOMContentLoaded', function () {
-     let headlines = document.querySelectorAll('.post-content h2[id], .post-content h3[id]');
+    let headlines = document.querySelectorAll('.post-content h2[id], .post-content h3[id]');
     console.log(headlines)
     for (let i = 0; i < headlines.length; i++) {
         let link = document.createElement('a')
@@ -46,16 +46,16 @@ document.addEventListener('DOMContentLoaded', function () {
 
     let projectImages = document.querySelectorAll('.project-image');
     let projectImageOverlay = document.querySelector('.image-fullscreen');
-    if (projectImageOverlay){
+    if (projectImageOverlay) {
 
-        projectImageOverlay.addEventListener('click', function(){
+        projectImageOverlay.addEventListener('click', function () {
             this.classList.remove('active');
         })
     }
-    
-    for (let i = 0; i < projectImages.length; i++){
+
+    for (let i = 0; i < projectImages.length; i++) {
         let pi = projectImages[i];
-        pi.addEventListener('click', function(e){
+        pi.addEventListener('click', function (e) {
             projectImageOverlay.classList.add('active');
             console.log(this);
             projectImageOverlay.style.backgroundImage = 'url(' + this.getAttribute('src') + ')';
@@ -65,12 +65,62 @@ document.addEventListener('DOMContentLoaded', function () {
     // Sort out home hover
     let homeItems = document.querySelectorAll('.project-list-item, .post-list-item');
     console.log(homeItems)
-    for (let i = 0; i < homeItems.length; i++){
-        homeItems[i].addEventListener('mouseover', function(){
+    for (let i = 0; i < homeItems.length; i++) {
+        homeItems[i].addEventListener('mouseover', function () {
             let year = this.getAttribute('data-year');
             this.parentNode.setAttribute('data-active-year', year);
         })
     }
-    
-    
+
+    let goods = document.querySelectorAll('.good');
+    let state = {
+        goodActive: false,
+    }
+    let allAnimationImages = document.querySelectorAll('.good-image-animation');
+    let openGood = function (activeGood) {
+        let animationImage = activeGood.querySelector('.good-image-animation');
+        state.goodActive = true;
+        animationImage.style.zIndex = 200;
+        document.body.classList.add('good-active')
+        let box = animationImage.getBoundingClientRect();
+        let scaleFactor = (window.innerHeight / box.height) * .95;
+        let h = box.height * scaleFactor;
+        let w = box.width * scaleFactor;
+        xReference = window.innerWidth / 2 + 100;
+        let offsetTop = -(box.top - ((h - box.height) / 2)) * .95;
+        let offsetLeft = (xReference - box.left) - w / 2;
+        animationImage.style.transform = 'translateX(' + offsetLeft + 'px) translateY(' + offsetTop + 'px) scale(' + scaleFactor + ')';
+        activeGood.classList.add('active')
+    }
+    let closeGood = function (activeGood) {
+        let animationImage = activeGood.querySelector('.good-image-animation');
+        document.body.classList.remove('good-active')
+        activeGood.classList.remove('active')
+        animationImage.style.transform = '';
+        state.goodActive = false;
+        setTimeout(function () {
+            for (let i = 0; i < allAnimationImages.length; i++) {
+                allAnimationImages[i].style.zIndex = 0;
+            }
+        }, 600)
+    }
+    for (let i = 0; i < goods.length; i++) {
+        goods[i].addEventListener('click', function () {
+            if (state.goodActive) {
+                closeGood(this)
+            } else {
+                openGood(this);
+            }
+        })
+    }
+
+    let closeGoodButton = document.querySelector('#close-good');
+    closeGoodButton.addEventListener('click', function (e) {
+        e.preventDefault();
+        let image = document.querySelector('.good.active');
+        console.log(image)
+        closeGood(image);
+    })
+
+
 }, false);

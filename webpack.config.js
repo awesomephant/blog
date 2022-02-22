a78d3502e4c6df390499fe62a555f8cdfbb6f277
Max Kohler <max@koehler-kn.de>
2022-02-22 19:08:43 +0100

    Remove CMS demo

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 module.exports = {
     entry: {
         main: './js/index.js',
-        cmsDemo: './js/cms-demo.js'
     },
     output: {
         filename: '[name].js',

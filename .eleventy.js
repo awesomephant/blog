07c3a9815d853c6ff476838e53102c14b6aa5ee0
Max Kohler <hello@maxkohler.com>
2022-06-11 10:00:07 +0200

    Fix import

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,7 @@ const taskLists = require("markdown-it-task-lists");
 const anchor = require("markdown-it-anchor");
 let footnotes = require("markdown-it-footnote");
 const typesetPlugin = require("eleventy-plugin-typeset");
-const csv = require('csv-parse/sync');
+const { parse } = require("csv-parse/sync");
 
 let markdownOptions = {
   html: true,
@@ -97,7 +97,7 @@ module.exports = function (eleventyConfig) {
     })
   );
   eleventyConfig.addDataExtension("csv", (contents) => {
-    const records = csv.parse(contents, {
+    const records = parse(contents, {
       columns: true,
       skip_empty_lines: true,
     });
